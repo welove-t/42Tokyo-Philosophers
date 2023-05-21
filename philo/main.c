882d34cade6f2d334a2281108284ded1eb3c170a
love <tlove.tky@gmail.com>
2023-05-21 15:52:48 +0900

    update:free free対応

--- philo/main.c
+++ philo/main.c
@@ -6,11 +6,30 @@
 /*   By: terabu <terabu@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/08 16:58:52 by terabu            #+#    #+#             */
-/*   Updated: 2023/05/21 11:35:33 by terabu           ###   ########.fr       */
+/*   Updated: 2023/05/21 15:07:50 by terabu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "philo.h"
+
+int	release(t_env *env)
+{
+	int	num_philo;
+	int	i;
+
+	num_philo = env->args->num_philo;
+	i = 0;
+	free(env->args);
+	free(env->mtx_fork);
+	free(env->mtx_meal);
+	free(env->philo);
+	// while (i < num_philo)
+	// {
+	// 	free()
+	// 	i++;
+	// }
+	return (0);
+}
 
 int	main(int argc, char *argv[])
 {
@@ -21,5 +40,12 @@ int	main(int argc, char *argv[])
 	if (init_philo(&env))
 		return (1);
 	create_thread(&env);
+	release(&env);
+
 	return (0);
 }
+
+// __attribute((destructor))
+// static void destructor() {
+//     system("leaks -q philo");
+// }
